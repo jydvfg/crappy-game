69ef855ec1b3f7b60682a53002e03a2f5d4401e7
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-01-26 07:21:27 +0100

    turn off debug mode

--- src/start.js
+++ src/start.js
@@ -9,7 +9,7 @@ var config = {
     default: "arcade",
     arcade: {
       gravity: { y: 0 },
-      debug: true, // set to true to view zones
+      debug: false, // set to true to view zones
     },
   },
   scene: [
